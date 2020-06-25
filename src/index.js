d00912a1dd992afad4369caeb0173ec961012d76
Yuri <yuri.camara15@gmail.com>
2020-06-24 22:25:44 -0300

    adicionando variaveis de ambiente

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ const mongoose = require('mongoose');
 //Criando uma instancia do express pra variavel app conduzir o sistema
 const app = express();
 
+//Criando um buscador para variaveis de ambiente
+require('dotenv').config('./.env');
+
+//Setando variavel de ambiente
+const mongoURL = process.env.MONGO_HOME;
+
 //Conectando ao MongoDBAtlas (servidor mongo em cloud)
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 //Definir como json o corpo das requisiçções nas API's
 app.use(express.json());
