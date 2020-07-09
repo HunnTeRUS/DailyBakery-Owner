7d5c0ffe61f6a3e577e5b819ba59fa6fe95efada
HunnTeRUS <otavio201378@gmail.com>
2020-07-09 01:34:29 -0300

    Criando email template para o metodo  "ForgotPassword"

--- src/services/utils/Mailer.js
+++ src/services/utils/Mailer.js
@@ -1,6 +1,13 @@
 var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var handlebars = require('handlebars');
+var fs = require('fs');
 
 require('dotenv').config('../../../.env');
+
+function gerarPassword() {
+    return Math.random().toString(36).slice(-10);
+}
 
 var remetente = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -12,30 +19,47 @@ var remetente = nodemailer.createTransport({
     }
 });
 
-function gerarPassword() {
-    return Math.random().toString(36).slice(-10);
-}
+var readHTMLFile = function(path, callback) {
+    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+        if (err) {
+            throw err;
+            callback(err);
+        }
+        else {
+            callback(null, html);
+        }
+    });
+};
+
+
 
 module.exports = {
     sendNewPasswordCodeByEmail(email) {
-        const codigoaenviar = gerarPassword();
+        
+        var code = gerarPassword();
 
-        var emailASerEnviado = {
-            from: String(process.env.EMAIL),
-            to: email,
-            subject: 'Codigo para atualização de senha',
-            text: `O codigo para alterar sua senha no app DailyBakery Owner é esse: ${codigoaenviar}`,
-        };
-
-        remetente.sendMail(emailASerEnviado, function (error) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email enviado com sucesso');
-            }
+        readHTMLFile('src/templates/emails/forgotPassword/forgotPass.html', function(err, html) {
+            var template = handlebars.compile(html);
+            var replacements = {
+                 code: code
+            };
+            var htmlToSend = template(replacements);
+            var mailOptions = {
+                from: String(process.env.EMAIL),
+                to: email,
+                subject: 'Codigo para atualização de senha',
+                html : htmlToSend
+             };
+            remetente.sendMail(mailOptions, function (error, response) {
+                if (error) {
+                    console.log(error);
+                    callback(error);
+                }
+            });
+            
         });
-
-        return codigoaenviar;
+                
+        return code;
     }
 }
 
