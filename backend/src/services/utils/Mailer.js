b5aa520501413fc99a75135ade7ae4c62fca67dc
Yuri <yuri.camara15@gmail.com>
2020-11-04 16:46:54 -0300

    Alterando envio de senha para 6 numeros

--- backend/src/services/utils/Mailer.js
+++ backend/src/services/utils/Mailer.js
@@ -6,7 +6,11 @@ var fs = require('fs');
 require('dotenv').config('../../../.env');
 
 function gerarPassword() {
-    return Math.random().toString(36).slice(-10);
+    let total
+    do {
+        total = Math.floor(Math.random() * 1000000)
+    } while (total < 100000 || total > 1000000)
+    return total.toString()
 }
 
 var remetente = nodemailer.createTransport({
@@ -20,12 +24,11 @@ var remetente = nodemailer.createTransport({
 });
 
 var readHTMLFile = function(path, callback) {
-    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+    fs.readFile(path, { encoding: 'utf-8' }, function(err, html) {
         if (err) {
             throw err;
             callback(err);
-        }
-        else {
+        } else {
             callback(null, html);
         }
     });
@@ -35,30 +38,29 @@ var readHTMLFile = function(path, callback) {
 
 module.exports = {
     sendNewPasswordCodeByEmail(email) {
-        
+
         var code = gerarPassword();
 
         readHTMLFile('src/templates/emails/forgotPassword/forgotPass.html', function(err, html) {
             var template = handlebars.compile(html);
             var replacements = {
-                 code: code
+                code: code
             };
             var htmlToSend = template(replacements);
             var mailOptions = {
                 from: String(process.env.EMAIL),
                 to: email,
                 subject: 'Codigo para atualização de senha',
-                html : htmlToSend
-             };
-            remetente.sendMail(mailOptions, function (error, response) {
+                html: htmlToSend
+            };
+            remetente.sendMail(mailOptions, function(error, response) {
                 if (error) {
                     console.log(error);
                 }
             });
-            
+
         });
-                
+
         return code;
     }
 }
-
