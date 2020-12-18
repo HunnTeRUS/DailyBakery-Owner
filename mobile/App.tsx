40b64e93d12558e790f063061d23c117c2c3d7ad
HunnTeRUS <otavio201378@gmail.com>
2020-12-17 23:31:43 -0300

    Corrigindo erro na execução do app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import * as Notifications from 'expo-notifications';
+import Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
 import useCachedResources from './src/hooks/useCachedResources';
 import useColorScheme from './src/hooks/useColorScheme';
 import Navigation from './src/navigation';
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
-
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
 
   if (!isLoadingComplete) {
     return null;
